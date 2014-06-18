e1900c0c71b8bb107860dcec2eea7ec3ab122eab
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-18 14:58:57 -0500

    add all files in

--- detection.py
+++ detection.py
@@ -2,5 +2,7 @@ import obspy as obs
 
 """read in the SAC files"""
 bhe_stream = obs.read('M43A/TA.M43A..BHE.M.2012.169.201502.SAC')
+bhn_stream = obs.read('M43A/TA.M43A..BHN.M.2012.169.201502.SAC')
+bhz_stream = obs.read('M43A/TA.M43A..BHZ.M.2012.169.201502.SAC')
 
 print bhe_stream
