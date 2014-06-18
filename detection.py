6eb5c65b6cf511f0b485794b99b1eb8daf703218
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-18 15:28:35 -0500

    perform network autocorrelation

--- detection.py
+++ detection.py
@@ -1,8 +1,8 @@
 import obspy as obs
+from networkAutocorrelation import NetworkAutocorrelation
 
 """read in the SAC files"""
-bhe_stream = obs.read('M43A/TA.M43A..BHE.M.2012.169.201502.SAC')
-bhn_stream = obs.read('M43A/TA.M43A..BHN.M.2012.169.201502.SAC')
-bhz_stream = obs.read('M43A/TA.M43A..BHZ.M.2012.169.201502.SAC')
+bhe_stream = obs.read('M43A/TA.M43A..BHE.M.2012.169.201502.SAC')[0]
+bhn_stream = obs.read('M43A/TA.M43A..BHN.M.2012.169.201502.SAC')[0]
+bhz_stream = obs.read('M43A/TA.M43A..BHZ.M.2012.169.201502.SAC')[0]
 
-print bhe_stream
